eaeaa3b62d0eebfd6aa63e96c5ef4cf08a068632
Vad3s <167074233+Vad3s@users.noreply.github.com>
2024-06-05 11:25:15 +0200

    Projekt erstellt

---
+++ README.md
@@ -0,0 +1,3 @@
+# list_screen_checkliste_1_5_8_1
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/list_screen_checkliste_1_5_8_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.list_screen_checkliste_1_5_8_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
